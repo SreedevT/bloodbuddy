1c7a4560542f838a53b75b80a7772b240b07fb1d
Sreedev <talisreedev@gmail.com>
2023-05-18 01:16:27 +0530

    Initial Phone Auth commit

---
+++ lib/authentication/phone_signup.dart
@@ -0,0 +1,140 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class MyPhone extends StatefulWidget {
+  const MyPhone({Key? key}) : super(key: key);
+
+  static String verificationId = " ";
+
+  @override
+  State<MyPhone> createState() => _MyPhoneState();
+}
+
+class _MyPhoneState extends State<MyPhone> {
+  TextEditingController countryController = TextEditingController();
+
+  String phone = "";
+
+  @override
+  void initState() {
+    countryController.text = "+91";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image.asset(
+              //   'assets/img1.png',
+              //   width: 150,
+              //   height: 150,
+              // ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                "Phone Verification",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "We need to register your phone without getting started!",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 55,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    SizedBox(
+                      width: 40,
+                      child: TextField(
+                        controller: countryController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      "|",
+                      style: TextStyle(fontSize: 33, color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                        child: TextField(
+                      onChanged: (value) {
+                        phone = value;
+                      },
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Phone",
+                      ),
+                    ))
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade600,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.verifyPhoneNumber(
+                        phoneNumber: '${countryController.text + phone}',
+                        verificationCompleted:
+                            (PhoneAuthCredential credential) {
+                          log('Verification Complete!!!!!!!!!!!');
+                        },
+                        verificationFailed: (FirebaseAuthException e) {
+                          log(e.toString());
+                        },
+                        codeSent: (String verificationId, int? resendToken) {
+                          MyPhone.verificationId = verificationId;
+                          Navigator.pushNamed(context, 'verify');
+                        },
+                        codeAutoRetrievalTimeout: (String verificationId) {},
+                      );
+                    },
+                    child: const Text("Send the code")),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
-
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:blood/authentication/register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
+import 'authentication/phone_signup.dart';
+import 'authentication/verify.dart';
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+    // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. debug provider
+    // 2. safety net provider
+    // 3. play integrity provider
+    androidProvider: AndroidProvider.debug,
+  );
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SignUpScreen()
-    );
-  }
+  runApp(MaterialApp(
+    initialRoute: 'phone_signup',
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'phone_signup': (context) => MyPhone(),
+      'verify': (context) => MyVerify(),
+      'register_test': (context) => SignUpScreen(),
+    },
+  ));
 }
