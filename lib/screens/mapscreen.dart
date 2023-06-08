184ceb1bf83b7b8a0656b0e7f18ce870f2301493
Sreedev <talisreedev@gmail.com>
2023-06-08 23:29:50 +0530

    Modified location picker

--- lib/screens/mapscreen.dart
+++ lib/screens/mapscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -10,25 +11,60 @@ class NewInter extends StatefulWidget {
 }
 
 class _NewInterState extends State<NewInter> {
-  Position? position; // position details to pass to OSM
-  bool isSeeLocationInMapEnabled = false; // this allows  to use the disable/enable button
+  LatLong? latLong;
+  String location = '';
+  bool _load = false;
 
-  // function to ge the current latitude and longitude
-  void getCurrentPosition() async {
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied ||
-        permission == LocationPermission.deniedForever) {
-      await Geolocator.requestPermission();
-    } else {
-      Position userPosition = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best);
-      setState(() {
-        position = userPosition;
-        isSeeLocationInMapEnabled = true; // This is set to true so once latlong is accessed, view location in map button can be enabled
-        print(position?.latitude.toString());
-        print(position?.longitude.toString());
-      });
+  Future<void> _getCurrentLocation() async {
+    Position position = await _determinePosition();
+
+    setState(() {
+      latLong = LatLong(position.latitude, position.longitude);
+    });
+  }
+
+  //? From https://pub.dev/packages/geolocator
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
     }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _setLoadingState(bool load) {
+    setState(() {
+      _load = load;
+    });
   }
 
   @override
@@ -42,21 +78,25 @@ class _NewInterState extends State<NewInter> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              height: 350,
+              height: 300,
               margin: const EdgeInsets.only(
-                  top: 30, left: 20, right: 20, bottom: 20),
+                  top: 20, left: 20, right: 20, bottom: 20),
               decoration: BoxDecoration(
                   // shape also can be specified like this => shape: BoxShape.circle,
-                  color: const Color.fromARGB(255, 129, 36,30), // use decoration for giving both color and radius
-                  borderRadius: BorderRadius.circular(20), // otherwise, if we give it separately, error will show up
+                  color: const Color.fromARGB(255, 129, 36,
+                      30), // use decoration for giving both color and radius
+                  borderRadius: BorderRadius.circular(
+                      20), // otherwise, if we give it separately, error will show up
                   border: Border.all(
                     color: Colors.white,
                     width: 1,
                   ),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withOpacity(1.0), // here, I set opacity to maximum
-                      spreadRadius:3, // this is to specify how much spread the shadow
+                      color: Colors.black
+                          .withOpacity(1.0), // here, I set opacity to maximum
+                      spreadRadius:
+                          3, // this is to specify how much spread the shadow
                       blurRadius: 10, // how much blur the shadow
                       //  offset: Offset(0, 3),
                     ),
@@ -75,7 +115,7 @@ class _NewInterState extends State<NewInter> {
                     ),
                     const SizedBox(height: 20),
                     const Text(
-                      "Please set your location by clicking the below shown button.",
+                      "Please set your location by clicking the button below.",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16,
@@ -92,59 +132,94 @@ class _NewInterState extends State<NewInter> {
                         ),
                         SizedBox(width: 10),
                         Text(
-                          "The location you set will be used to show\n you blood requests.",
+                          "We don't share your location with anyone.",
                           style: TextStyle(
                             color: Colors.white,
                           ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: getCurrentPosition,
-                          child: const Text("Set Current Location"),
-                        ),
-                        ElevatedButton(
-                          onPressed: isSeeLocationInMapEnabled?
-                              () {
-                                  showModalBottomSheet(
-                                      context: context,
-                                      barrierColor: Colors.black.withOpacity(
-                                          0.7), // so what this does is that it gives this color to main screen when the sheet pops up
-                                      builder: (BuildContext context) {
-                                        return Container(
-                                            height: 500,
-                                            child: OpenStreetMapSearchAndPick(
-                                              center: LatLong(
-                                                  position!.latitude,
-                                                  position!.longitude),
-                                              buttonColor: const Color.fromARGB(
-                                                  255, 129, 36, 30),
-                                              onPicked: (PickedData) {
-                                                setState(() {
-                                                  print(PickedData.address);
-                                                });
-                                                Navigator.pop(
-                                                    context); // this enables  to close the bottom sheet when this button is clicked
-                                              },
-                                            ));
-                                      });
-                                }
-                              : null,
-                          child: const Text(
-                            "See location in Map",
+                    // SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateColor.resolveWith(
+                            (states) => Colors.white
+                            ), // by writing this instead of materialstateproperty, we can give dynamic colors
+                        overlayColor: MaterialStateColor.resolveWith((states) =>
+                            Colors.green.withOpacity(
+                                0.3)), // like here the button color is changed, when we tap it. COOL ;)
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Set Current Location",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 129, 36, 30),
+                            ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(width: 10),
+                          _load
+                              ? const SizedBox(
+                                height: 15,
+                                width: 15,
+                                child: CircularProgressIndicator())
+                              : const SizedBox(
+                                  height: 0,
+                                  width: 0,
+                                ),
+                        ],
+                      ),
+                      onPressed: () async {
+                        _setLoadingState(true);
+                        await _getCurrentLocation()
+                            .whenComplete(() => _setLoadingState(false));
+
+                        if (!mounted) return;
+                        _showModalBottomSheet(context);
+                      },
                     ),
                   ],
                 ),
               ),
             ),
+            const SizedBox(height: 20),
+            Text(
+              location,
+              style: const TextStyle(
+                color: Color.fromARGB(255, 129, 36, 30),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            // const SizedBox(height: 10,),
+            // ElevatedButton(onPressed: (){}, child: Text("Next")),
           ],
         ));
+  }
+
+  void _showModalBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        barrierColor: Colors.black.withOpacity(
+            0.7), // so what this does is that it gives this color to main screen when the sheet pops up
+        builder: (BuildContext context) {
+          return SizedBox(
+            height: 500,
+            child: OpenStreetMapSearchAndPick(
+              center: latLong ?? LatLong(0, 0),
+              buttonColor: const Color.fromARGB(255, 129, 36, 30),
+              onPicked: (PickedData pickedData) {
+                setState(() {
+                  location = pickedData.address;
+                });
+                Navigator.pop(
+                    context); // this enables  to close the bottom sheet when this button is clicked
+              },
+            ),
+          );
+        });
   }
 }
