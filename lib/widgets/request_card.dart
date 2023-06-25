d60ad118bbdf9203efca1b7cea5e97b8ab70f153
Sreedev <talisreedev@gmail.com>
2023-06-25 20:51:06 +0530

    animated fade in

--- lib/widgets/request_card.dart
+++ lib/widgets/request_card.dart
@@ -2,13 +2,12 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-class BloodRequestCard extends StatelessWidget {
+class BloodRequestCard extends StatefulWidget {
   final String id;
   final String hospital;
   final int units;
   final String bloodGroup;
   final String name;
-  // final String phone;
 
   const BloodRequestCard({
     Key? key,
@@ -17,56 +16,81 @@ class BloodRequestCard extends StatelessWidget {
     required this.units,
     required this.bloodGroup,
     required this.name,
-    // required this.phone,
   }) : super(key: key);
 
   @override
+  _BloodRequestCardState createState() => _BloodRequestCardState();
+}
+
+class _BloodRequestCardState extends State<BloodRequestCard> {
+  // A boolean to track the visibility of the card
+  bool _visible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Set the visibility to true after a small delay
+    Future.delayed(const Duration(milliseconds: 100), () {
+      setState(() {
+        _visible = true;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      color: Colors.red[100],
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              hospital,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    return AnimatedOpacity(
+      // Use the _visible value to determine the opacity
+      opacity: _visible ? 1.0 : 0.0,
+      // Use a duration of 500 milliseconds for the animation
+      duration: const Duration(milliseconds: 500),
+      // Use a linear curve for the animation
+      curve: Curves.linear,
+      // The child widget is your card
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        color: Colors.red[100],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.hospital,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              '$units units of $bloodGroup blood needed for $name',
-              style: const TextStyle(
-                fontSize: 16,
+              const SizedBox(height: 4),
+              Text(
+                '${widget.units} units of ${widget.bloodGroup} blood needed for ${widget.name}',
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
               ),
-            ),
-            const SizedBox(height: 4),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // call the phone number
-                    log("Phone Icon Pressed, call the phone number");
-                  },
-                  icon: const Icon(Icons.call),
-                  label: Text("Call $name"),
-                  style: ElevatedButton.styleFrom(
-                    surfaceTintColor: Colors.red[300],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+              const SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // call the phone number
+                    },
+                    icon: const Icon(Icons.call),
+                    label: Text('Call ${widget.name}'),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
