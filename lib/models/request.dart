fa07742b971c9b06d87a345362e9c19ec968ef82
Sreedev <talisreedev@gmail.com>
2023-06-27 03:36:11 +0530

    Proposed request model

---
+++ lib/models/request.dart
@@ -0,0 +1,71 @@
+enum Status { pending, accepted }
+
+class Request  {
+  //? ID of the user making the request
+  // each user can have multiple requests
+  // we can identify the user by this ID
+  final String id; 
+
+  final String hospitalName;
+  final String bloodGroup;
+  final int units;
+  //? name of the patient for whom the request is made
+  final String patientName;
+  final String area;
+  // var requisitionForm; //? image/pdf of the requisition form
+  final DateTime expiryDate;
+
+  //? emergency requests treated differenty
+  // maybe shown in a different screen / different color / highlighted
+  final bool isEmergency;
+  Status status;
+
+  Request({
+    required this.id,
+    required this.hospitalName,
+    required this.bloodGroup,
+    required this.units,
+    required this.patientName,
+    required this.area,
+    required this.expiryDate,
+    this.isEmergency = false,
+    this.status = Status.pending,
+  });
+  
+  //? convert the request object to a map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'hospitalName': hospitalName,
+      'bloodGroup': bloodGroup,
+      'units': units,
+      'patientName': patientName,
+      'area': area,
+      'expiryDate': expiryDate,
+      'isEmergency': isEmergency,
+      'status': status.name,
+    };
+  }
+
+  //? convert the map to a request object
+  factory Request.fromMap(Map<String, dynamic> map) {
+    return Request(
+      id: map['id'],
+      hospitalName: map['hospitalName'],
+      bloodGroup: map['bloodGroup'],
+      units: map['units'],
+      patientName: map['patientName'],
+      area: map['area'],
+      expiryDate: map['expiryDate'].toDate(),
+      isEmergency: map['isEmergency'],
+      //! not sure if this will work
+      status: Status.values.firstWhere((element) => element.name == map['status']),
+    );
+  }
+
+  //? convert to string
+  @override
+  String toString() {
+    return '$patientName needs $units units of $bloodGroup blood at $hospitalName in $area';
+  }
+}
