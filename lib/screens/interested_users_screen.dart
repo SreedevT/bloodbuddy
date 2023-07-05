ec6973f73c9f47972bb84736690bb8d96ad361bc
Sreedev <talisreedev@gmail.com>
2023-07-06 03:46:34 +0530

    Updated user contact UI, renamed files

--- lib/screens/interested_users_screen.dart
+++ lib/screens/interested_users_screen.dart
@@ -1,5 +1,5 @@
 import 'package:blood/widgets/info_text.dart';
-import 'package:blood/widgets/list.dart';
+import 'package:blood/widgets/interested_users_list.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
--- lib/screens/my_requests_screen.dart
+++ lib/screens/my_requests_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:blood/widgets/interestedlist.dart';
+import 'package:blood/screens/interested_users_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
