4d02cd96b110f080c009f8c7db242ce5e1390db1
Sreedev <talisreedev@gmail.com>
2023-07-09 02:31:35 +0530

    Some Faq warning removed

--- lib/screens/Faq.dart
+++ lib/screens/Faq.dart
@@ -13,7 +13,7 @@ class _FAQPageState extends State<FAQPage> {
       appBar: AppBar(
         toolbarHeight: 60.0,
         elevation: 0,
-        title: Text(
+        title: const Text(
           "FAQS",
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
@@ -31,7 +31,7 @@ class _FAQPageState extends State<FAQPage> {
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Color.fromARGB(255, 231, 231, 231),
           // borderRadius: BorderRadius.only(
           //   topLeft: Radius.circular(30),
@@ -46,13 +46,13 @@ class _FAQPageState extends State<FAQPage> {
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Who can donate blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -63,14 +63,14 @@ class _FAQPageState extends State<FAQPage> {
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Generally, individuals who are in good health and meet the specific eligibility criteria set by blood service organizations can donate blood.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -82,13 +82,13 @@ class _FAQPageState extends State<FAQPage> {
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'How often can I donate blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -99,14 +99,14 @@ class _FAQPageState extends State<FAQPage> {
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Donors can donate whole blood every 8 to 12 weeks (approximately 56 to 84 days).',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -118,13 +118,13 @@ class _FAQPageState extends State<FAQPage> {
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'How long does a blood donation take?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -135,14 +135,14 @@ class _FAQPageState extends State<FAQPage> {
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'The entire blood donation process typically takes around 30 to 60 minutes, although it may vary depending on factors such as individual circumstances, the type of donation, and the efficiency of the blood center or organization.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -154,13 +154,13 @@ class _FAQPageState extends State<FAQPage> {
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Are there any side effects of donation?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -171,14 +171,14 @@ class _FAQPageState extends State<FAQPage> {
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'While most donors experience no adverse effects, some may encounter minor side effects such as lightheadedness, dizziness, or bruising at the needle insertion site. These symptoms are usually temporary and resolve quickly. Blood collection centers have trained staff to handle any complications that may arise.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -190,13 +190,13 @@ class _FAQPageState extends State<FAQPage> {
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'How can I prepare to donate blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -207,14 +207,14 @@ class _FAQPageState extends State<FAQPage> {
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Eat at your regular mealtimes and drink plenty of fluid before you donate blood. Have a snack at least four hours before you donate, but do not eat too much right before the donation.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -226,13 +226,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Can I donate if I am taking medicine?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -243,14 +243,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Most medicines do not prevent you from donating blood. Common medicines — such as those used to control blood pressure, birth control pills and the type of medicines you can get without a prescription — do not affect your eligibility.If you are taking antibiotics to treat a current infection, you must complete the course before donating. ',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -262,13 +262,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'What is a “unit” of blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -279,14 +279,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'A unit is about 450 ml of donated blood. The average adult has between four and five litres of blood in his or her body, and can easily spare one unit.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -298,13 +298,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Can I donate if I have diabetes?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -315,14 +315,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'If your diabetes is being treated and is under control, you are most likely able to donate blood. You should let your doctor know that you plan to donate.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -334,13 +334,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'What is the rarest blood type?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -351,14 +351,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'AB negative is the rarest of the eight main blood types - just 1% of our donors have it.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -370,13 +370,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         ' Why I have to wait between donations?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -387,14 +387,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'While the plasma is replenished quickly, it can take four to six weeks for your body to manufacture the red blood cells that are lost. If you are only donating platelets, which your body replaces within a day, you can give again after a week. However, you are restricted to only 24 total platelet donations a year.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -406,13 +406,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Is it safe to donate blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -423,14 +423,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Yes, donating blood is generally considered safe. Blood collection is conducted using sterile equipment, and all necessary precautions are taken to ensure donor safety and minimize any potential risks.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -442,13 +442,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Can I donate blood if I have a tattoo?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -459,14 +459,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'There may be a deferral period after getting a tattoo or body piercing,this is to minimize the risk of bloodborne infections. The deferral period can range from a few months to a year or more.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -478,13 +478,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Can I donate if recently I had surgery?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -495,14 +495,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'The eligibility to donate blood after surgery depends on various factors such as the type of surgery, the recovery period, and individual health. In most cases, there is a waiting period before individuals can donate blood after surgery to ensure proper healing and recovery. The waiting period can range from a few weeks to several months.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
@@ -514,13 +514,13 @@ ExpansionTile(
       Container(
         height: 12.0,
         width: 12.0,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green,
           shape: BoxShape.circle,
         ),
       ),
-      SizedBox(width: 8.0),
-      Text(
+      const SizedBox(width: 8.0),
+      const Text(
         'Are there any benefits to donating blood?',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -531,14 +531,14 @@ ExpansionTile(
   ),
   children: <Widget>[
     Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Yes, donating blood can have several benefits. It provides an opportunity to help others and contribute to the community. Additionally, regular blood donation may help in identifying potential health issues as donors undergo a screening process that includes checking for various health parameters such as blood pressure, hemoglobin levels, and infectious diseases.',
           ),
-          SizedBox(height: 8.0), // Add space below the line
+          const SizedBox(height: 8.0), // Add space below the line
         ],
       ),
     ),
