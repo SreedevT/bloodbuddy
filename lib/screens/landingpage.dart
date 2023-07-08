0fb46f722aefba5e723962d002d30d978a4bc3fc
Sreedev <talisreedev@gmail.com>
2023-07-08 21:43:18 +0530

    added fade in animation to HomeScreen

--- lib/screens/landingpage.dart
+++ lib/screens/landingpage.dart
@@ -7,11 +7,11 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   late AnimationController _animationController;
   late Animation<double> _animation;
 
@@ -51,161 +51,164 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
-        elevation: 0,
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              color: Colors.white,
-              onPressed: () {
-                _scaffoldKey.currentState!.openDrawer();
-              },
-            );
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            color: Colors.white,
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
+    return FadeTransition(
+      opacity: _animation,
+      child: Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          elevation: 0,
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                color: Colors.white,
+                onPressed: () {
+                  _scaffoldKey.currentState!.openDrawer();
+                },
+              );
             },
           ),
-        ],
-      ),
-      drawer: drawer(context),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const DonorCard(),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 80, 0),
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: const Text(
-                  'Recent Donation',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              color: Colors.white,
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          ],
+        ),
+        drawer: drawer(context),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const DonorCard(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 80, 0),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: const Text(
+                    'Recent Donation',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
-                width: 350,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(212, 231, 231, 231),
-                  borderRadius: BorderRadius.circular(20),
+                const SizedBox(
+                  height: 20,
                 ),
-                child: const Text(
-                  'Sony donated to shaf......................',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 5, 0, 0),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
+                  width: 350,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(212, 231, 231, 231),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    'Sony donated to shaf......................',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 5, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
-                width: 350,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(212, 231, 231, 231),
-                  borderRadius: BorderRadius.circular(20),
+                const SizedBox(
+                  height: 10,
                 ),
-                child: const Text(
-                  'Sony donated to shaf......................',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 5, 0, 0),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
+                  width: 350,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(212, 231, 231, 231),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    'Sony donated to shaf......................',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 5, 0, 0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 1,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(150, 0, 0, 0),
-                child: const InkWell(
-                  child: Text(
-                    'Click here for more donation Details',
-                    style: TextStyle(fontSize: 9),
+                const SizedBox(
+                  height: 1,
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(150, 0, 0, 0),
+                  child: const InkWell(
+                    child: Text(
+                      'Click here for more donation Details',
+                      style: TextStyle(fontSize: 9),
+                    ),
                   ),
                 ),
-              ),
-              Image.asset(
-                'assets/images/image2.jpg',
-                width: 150,
-                height: 100,
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(50, 1, 50, 0),
-                color: const Color.fromARGB(211, 255, 255, 255),
-                child: const Text(
-                  'Thank you For donating to save a life',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 5, 0, 0),
-                    fontSize: 15,
+                Image.asset(
+                  'assets/images/image2.jpg',
+                  width: 150,
+                  height: 100,
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(50, 1, 50, 0),
+                  color: const Color.fromARGB(211, 255, 255, 255),
+                  child: const Text(
+                    'Thank you For donating to save a life',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 5, 0, 0),
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: true,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage("assets/images/donation.png"),
-              color: Colors.red,
-              size: 24,
+        bottomNavigationBar: BottomNavigationBar(
+          showUnselectedLabels: true,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-            label: 'Donation',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage("assets/images/blood.png"),
-              color: Colors.red,
-              size: 24,
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/images/donation.png"),
+                color: Colors.red,
+                size: 24,
+              ),
+              label: 'Donation',
             ),
-            label: 'Request',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_outlined),
-            label: 'Profile',
-          ),
-        ],
-        selectedItemColor: Colors.red[800],
-        unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
-        onTap: _onItemTapped,
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage("assets/images/blood.png"),
+                color: Colors.red,
+                size: 24,
+              ),
+              label: 'Request',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_outlined),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.red[800],
+          unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
@@ -255,7 +258,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
               title: const Text('Profile'),
               onTap: () {
                 Navigator.pop(context);
-                Navigator.pushNamed(context, '/profile');
+                Navigator.pushNamed(context, 'profile');
               },
             ),
             ListTile(
