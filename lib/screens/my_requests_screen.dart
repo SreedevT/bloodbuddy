17065a07a2556e3b708a206b69b071f3326e01f3
Sreedev <72468234+SreedevT@users.noreply.github.com>
2023-07-09 17:49:40 +0530

    Request card UI redisign. Fixed Isuue 20. Added reload Util to reload any page to setState (#21)

--- lib/screens/my_requests_screen.dart
+++ lib/screens/my_requests_screen.dart
@@ -50,65 +50,73 @@ class _MyRequestListState extends State<MyRequestList> {
             )
           ],
         ),
-        body: FutureBuilder<QuerySnapshot>(
-          future: query.get(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return const Text("Something went wrong");
-            }
-
-            // if (snapshot.hasData && !snapshot.data.docs.isEmpty) {
-            //   return Text("Document does not exist");
-            // }
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              for (var doc in snapshot.data!.docs) {
-                var data = doc.data() as Map<String, dynamic>;
-                log("added: $data");
-                //TODO add a delete button, a way to update request fields,
-                requests.add(MyRequestCard(
-                  id: doc.id,
-                  hospital: data['hospitalName'],
-                  units: data['units'],
-                  bloodGroup: data['bloodGroup'],
-                  name: data['patientName'],
-                  onDelete: () {
-                    //? This is a callback method
-                    // when widget is deleted ,it calls this method
-                    // and setState is invoked in the parent widget(ListView)
-                    // this reloads the page and deleted widget is removed.
-                    setState(() {});
-                  },
-                ));
+        body: RefreshIndicator(
+          onRefresh: () async {
+            setState(() {});
+          },
+          child: FutureBuilder<QuerySnapshot>(
+            future: query.get(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text("Something went wrong");
               }
 
-              return Container(
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 231, 231, 231),
-                ),
-                child: ListView.builder(
-                  itemCount: requests.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onTap: (() {
-                          Navigator.push(
+              // if (snapshot.hasData && !snapshot.data.docs.isEmpty) {
+              //   return Text("Document does not exist");
+              // }
+
+              if (snapshot.connectionState == ConnectionState.done) {
+                for (var doc in snapshot.data!.docs) {
+                  var data = doc.data() as Map<String, dynamic>;
+                  log("added: $data");
+                  //TODO add a delete button, a way to update request fields,
+                  requests.add(MyRequestCard(
+                    reqId: doc.id,
+                    hospitalAddress: data['hospitalName'],
+                    units: data['units'],
+                    bloodGroup: data['bloodGroup'],
+                    patientName: data['patientName'],
+                    status: data['status'],
+                    onDelete: () {
+                      //? This is a callback method
+                      // when widget is deleted ,it calls this method
+                      // and setState is invoked in the parent widget(ListView)
+                      // this reloads the page and deleted widget is removed.
+                      setState(() {});
+                    },
+                  ));
+                }
+
+                return Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 231, 231, 231),
+                  ),
+                  child: ListView.builder(
+                    itemCount: requests.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                          onTap: (() {
+                            Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => InterestedUsers(
-                                      reqid: requests[index].id)));
-                        }),
-                        child: requests[index]);
-                  },
-                ),
-              );
-            }
+                                builder: (context) => InterestedUsers(
+                                  reqid: requests[index].reqId,
+                                ),
+                              ),
+                            );
+                          }),
+                          child: requests[index]);
+                    },
+                  ),
+                );
+              }
 
-            return const Align(
-              alignment: Alignment.topCenter,
-              child: RefreshProgressIndicator(),
-            );
-          },
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
         ));
   }
 }
