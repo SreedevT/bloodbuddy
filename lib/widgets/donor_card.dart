017a35431ed9582cd531ee631dea4cdb6abdce96
Sreedev <talisreedev@gmail.com>
2023-08-16 15:51:38 +0530

    Updated Card UI

--- lib/widgets/donor_card.dart
+++ lib/widgets/donor_card.dart
@@ -1,7 +1,9 @@
-import 'dart:developer';
+import 'dart:developer' as dev;
+import 'dart:math';
 import 'package:blood/Firestore/userprofile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import '../models/profile.dart';
@@ -34,117 +36,148 @@ class _DonorCardState extends State<DonorCard> {
         setState(() {
           data = json;
         });
-        log("Profile donor_card: $data");
+        dev.log("Profile donor_card: $data");
       });
     } catch (e) {
-      log("Fetch profile of donor_card: ${e.toString()}");
+      dev.log("Fetch profile of donor_card: ${e.toString()}");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // width: 450.0,
-      // height: 220.0,
-      padding: const EdgeInsets.all(16.0),
       margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
       decoration: BoxDecoration(
-          color: Colors.red[800],
+          gradient: LinearGradient(
+            colors: [
+              Colors.red[800]!,
+              Color.fromARGB(255, 158, 25, 25),
+              Color.fromARGB(255, 158, 25, 25)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.74, 0.7401, 1],
+          ),
           borderRadius: BorderRadius.circular(20),
           border: Border.all(color: Colors.black, width: 2)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                color: Colors.white,
-                padding: const EdgeInsets.fromLTRB(4, 1, 4, 1),
-                child: Text("BB",
-                    style: TextStyle(
-                        color: Colors.red[800],
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic)),
-              )),
-          Row(
-            children: [
-              Image.asset('assets/images/thankshand.png', height: 90),
-              const SizedBox(width: 10),
-              const Text(
-                "\"I do something Amazing\n\t\t\t\tI give blood !\"",
-                style: TextStyle(
-                    letterSpacing: 2,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
+      child: Stack(
+        children: 
+          [
+            Image.asset('assets/path2717.png'),
+            Container(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      color: Colors.white,
+                      padding: const EdgeInsets.fromLTRB(4, 1, 4, 1),
+                      child: Text("BB",
+                          style: TextStyle(
+                              color: Colors.red[800],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic)),
+                    )),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/thankshand.png',
+                      height: 90,
+                    ),
+                    const SizedBox(width: 10),
+                    Column(
+                      children: const [
+                        Text(
+                          "\"I do something Amazing",
+                          style: TextStyle(
+                              letterSpacing: 2,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "I give blood!\"",
+                          style: TextStyle(
+                              letterSpacing: 2,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                bottomInfoBar()
+              ],
+            ),
           ),
-          const SizedBox(height: 10),
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text(
-                'Name',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300),
-              ),
-              Text(
-                'Blood Group',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300),
-              ),
-              Text(
-                'Donor No.',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 3,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '${data?['First Name'] ?? 'loading...'}',
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(
-                '  ${data?['Blood Group'] ?? 'loading...'}',
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(
-                "${data?['id'].substring(0, 6) ?? 'loading...'}",
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600),
-              ),
-            ],
-          )
         ],
       ),
+    );
+  }
+
+  Row bottomInfoBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            const Text(
+              'Name',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300),
+            ),
+            Text(
+              '${data?['First Name'] ?? 'loading...'}',
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            const Text(
+              'Blood Group',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300),
+            ),
+            Text(
+              '  ${data?['Blood Group'] ?? 'loading...'}',
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            const Text(
+              'Donor No.',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "${data?['id'].substring(0, 6) ?? 'loading...'}",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
